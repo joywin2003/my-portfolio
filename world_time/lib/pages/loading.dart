3a218cf87ce0f402779a74e80658bfa95d46eadb
joywin2003 <21a20.joywin@sjec.ac.in>
2023-04-18 19:31:05 +0530

    added datatime

--- world_time/lib/pages/loading.dart
+++ world_time/lib/pages/loading.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
 
 class Loading extends StatefulWidget {
   const Loading({Key? key}) : super(key: key);
@@ -8,6 +10,25 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+
+  void getTime() async {
+      Response response = await get(Uri.parse('http://worldtimeapi.org/api/timezone/Asia/Kolkata'));
+      Map data = jsonDecode(response.body);
+      String datetime = data['datetime'];
+      String offset = data['utc_offset'].substring(1,3);
+
+      DateTime now = DateTime.parse(datetime);
+      now = now.add(Duration(hours: int.parse(offset)));
+      print(now);
+  }
+
+
+  @override
+  void initState(){
+    super.initState();
+    getTime();
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
