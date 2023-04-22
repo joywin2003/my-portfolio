eda5e2525021523728302722f4450ba377c59621
joywin2003 <21a20.joywin@sjec.ac.in>
2023-04-22 17:19:08 +0530

    updated file

--- my_portfolio/lib/main.dart
+++ my_portfolio/lib/main.dart
@@ -1,115 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MaterialApp(
+      home: PortFolio()
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class PortFolio extends StatefulWidget {
+  const PortFolio({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  @override
+  State<PortFolio> createState() => _PortFolioState();
+}
+
+class _PortFolioState extends State<PortFolio> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: SafeArea(
+        child: GNav(
+            rippleColor: (Colors.grey[800])!, // tab button ripple color when pressed
+            hoverColor: (Colors.grey[700])!, // tab button hover color
+            haptic: true, // haptic feedback
+            tabBorderRadius: 15,
+            tabActiveBorder: Border.all(color: Colors.black, width: 1), // tab button border
+            tabBorder: Border.all(color: Colors.grey, width: 1), // tab button border
+            tabShadow: [BoxShadow(color: Colors.grey.withOpacity(0.5), blurRadius: 8)], // tab button shadow
+            curve: Curves.easeOutExpo, // tab animation curves
+            duration: const Duration(milliseconds: 900), // tab animation duration
+            gap: 8, // the tab button gap between icon and text
+            color: Colors.grey[800], // unselected icon color
+            activeColor: Colors.purple, // selected icon and text color
+            iconSize: 24, // tab button icon size
+            tabBackgroundColor: Colors.purple.withOpacity(0.1), // selected tab background color
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5), // navigation bar padding
+            tabs: const [
+              // GButton(
+              //   // icon: FontAwesomeIcons.house,
+              //   text: 'Home',
+              // ),
+              //   GButton(
+              //     icon: FontAwesomeIcons.house,
+              //     text: 'Likes',
+              //   ),
+              //   GButton(
+              //     icon: FontAwesomeIcons.addressBook,
+              //     text: 'Search',
+              //   ),
+              //   GButton(
+              //     icon: FontAwesomeIcons.blog,
+              //     text: 'blog',
+              //   )
+            ]
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
