0d67ec0208a00edb4dad250df70cb1ef21c9e3d3
Joywin Bennis <107112207+joywin2003@users.noreply.github.com>
2023-04-22 18:14:24 +0530

    Delete quotes directory

--- quotes/android/app/src/main/kotlin/com/example/quotes/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.quotes
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- quotes/lib/quote.dart
+++
@@ -1,6 +0,0 @@
-class Quote {
-  String text;
-  String author;
-
-  Quote({required this.text, required this.author});
-}
